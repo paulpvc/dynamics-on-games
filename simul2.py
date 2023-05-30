2d07883c2c9e4303bdbf53c46d9bd8a9a396c732
@BillKelly12 <kelly.goutchowanou@gmail.com>
2023-05-30 09:54:17 +0200

    determination de graphe de la dynamique P1

--- simul2.py
+++ simul2.py
@@ -2,19 +2,12 @@ import networkx as nx
 import my_networkx as my_nx
 import matplotlib.pyplot as plt
 from itertools import product
+from util import *
 
-def affichage(G):
-    pos = nx.spring_layout(G, seed=5)
-    fig, ax = plt.subplots()
-    nx.draw_networkx_nodes(G, pos, ax=ax)
-    nx.draw_networkx_labels(G, pos, ax=ax)
 
-    curved_edges = [edge for edge in G.edges()]
-    nx.draw_networkx_edges(G, pos, edgelist=curved_edges, connectionstyle='arc3, rad=0.25')
-    edge_weights = nx.get_edge_attributes(G, 'w')
-    curved_edge_labels = {edge: edge_weights[edge] for edge in curved_edges}
-    my_nx.my_draw_networkx_edge_labels(G, pos, ax=ax, edge_labels=curved_edge_labels, rotate=False, rad=0.25)
-    plt.show()
+
+
+
 def get_graph(nodes: list, edges: list[tuple]):
     graph = nx.DiGraph()
     graph.add_nodes_from(nodes)
@@ -27,57 +20,69 @@ def get_strategy_profiles(graph:nx.DiGraph):
      for node in list(graph.nodes):
          if graph.out_degree[node] > 0:
             players_actions[node] = list(graph.edges([node]))
-     strategy_profiles =  list((product(*players_actions.values())))
+     strategy_profiles =  [strategy_profile for strategy_profile in (product(*players_actions.values()))]
      return strategy_profiles
 
 
 def get_nodes_of_dynamic_graph(graph:nx.DiGraph):
     nodes = []
+    set_of_edges_label = set()
+    my_list = []
     node_label_content = []
     strategy_profiles = get_strategy_profiles(graph)
     for strategy_profile in strategy_profiles:
         for player_strategy in strategy_profile:
-            node_label_content.append(graph.get_edge_data(*player_strategy)["w"])
+            temp = graph.get_edge_data(*player_strategy)["w"]
+            node_label_content.append(temp)
+            set_of_edges_label.add(temp)
         nodes.append(''.join(node_label_content))
+        my_list.append(set_of_edges_label)
         node_label_content = []
-    return nodes
+        set_of_edges_label = set()
+    return nodes,my_list
 
 
+def get_edges_of_dynamic_P1_graph(graph:nx.DiGraph, preferences):
+     edges = []
+     nodes, my_list = get_nodes_of_dynamic_graph(graph)
+     for p in range(len(preferences)):
+         for i in range(len(my_list)):
+             for j in range(len(my_list)):
+                 if is_edge_in_P1(my_list[i],my_list[j],p+1,preferences[p]):
+                     edges.append((nodes[i],nodes[j]))
+     return edges
 
+def get_payoff(strategy:set, preference:list[set]):
+    payoff = -1
+    for i in range(len(preference)):
+        if preference[i].issubset(strategy):
+            payoff = i
+    return payoff
 
-
-"""def get_labelled_strategy_profiles(graph:nx.DiGraph):
-    strategy_profiles =get_strategy_profiles(graph)
-    labelled_strategy_profiles = []
-    for strategy_profile in strategy_profiles:
-        labels = set()
-        for player_strategy in strategy_profile:
-            labels.add(graph.get_edge_data(*(player_strategy))["w"])
-        labelled_strategy_profiles.append(labels)
-    return labelled_strategy_profiles"""
-
-def is_edge(node1, node2,pref):
-    if len(node1.intersection(node2)) == 1 and pref.index(node1) < pref.index(node2):
+def is_edge_in_P1(node1, node2,player,pref):
+    my_set = node1.intersection(node2)
+    if  len(my_set) == 1 and not list(my_set)[0].endswith(str(player))  and get_payoff(node1,pref) < get_payoff(node2,pref):
         return True
     return False
 
-"""def get_dynamic_P1_graph(graph:nx.DiGraph):
-    labelled_strategy_profiles = get_labelled_strategy_profiles(graph)
-    dynamic_P1_graph = nx.DiGraph
-    u = [''.join(list(e)) for e in labelled_strategy_profiles]
-    dynamic_P1_graph.add_nodes_from(graph1,u)
-    return dynamic_P1_graph """
 
 
 
 
 
-preferences_1 = [{"c1","c2"},{"s1,s2"},{"c1","s2"}]
-preferences_2 = [{"c2","c1"},{"s2,s1"},{"c2","s1"}]
+
+preferences_1 = [{"c1","c2"},{"s1"},{"c1","s2"}]
+preferences_2 = [{"c2","c1"},{"s2"},{"c2","s1"}]
+preferences = [preferences_1,preferences_2]
 
 graph1 = get_graph([1,2,3],[(1,2,{"w": "c1"}),(2,1,{"w": "c2"}),(1,3,{"w": "s1"}),(2,3,{"w": "s2"})])
-print(get_strategy_profiles(graph1))
-print(get_nodes_of_dynamic_graph(graph1))
+
+print(get_edges_of_dynamic_P1_graph(graph1,preferences))
+
+graph2 = get_graph(get_nodes_of_dynamic_graph(graph1)[0],get_edges_of_dynamic_P1_graph(graph1,preferences))
+
+print(graph2)
+affichage_dyna(graph2)
 
 
 
