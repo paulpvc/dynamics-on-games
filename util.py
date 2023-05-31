73ddc58045f1c3781226800a80b8eb77d81a7d89
paulv <paul.vigeolas@gmail.com>
2023-05-31 14:01:35 +0200

    title to plot

--- util.py
+++ util.py
@@ -3,7 +3,7 @@ import my_networkx as my_nx
 import matplotlib.pyplot as plt
 
 
-def affichage(G):
+def affichage(G, title=""):
     """
     affichage d'un graphe orienté avec des noms pour les arcs
     :param G: graphe à afficher (arcs possédant tous des noms)
@@ -19,9 +19,10 @@ def affichage(G):
     edge_weights = nx.get_edge_attributes(G, 'w')
     curved_edge_labels = {edge: edge_weights[edge] for edge in curved_edges}
     my_nx.my_draw_networkx_edge_labels(G, pos, ax=ax, edge_labels=curved_edge_labels, rotate=False, rad=0.25)
+    plt.title(title)
     plt.show()
 
-def affichage_dyna(G):
+def affichage_dyna(G, title=""):
     """
     affichage d'un graphe orienté pour les dynamiques (arcs sans nom)
     :param G: graphe d'une dynamique à afficher
@@ -34,5 +35,5 @@ def affichage_dyna(G):
 
     curved_edges = [edge for edge in G.edges()]
     nx.draw_networkx_edges(G, pos, edgelist=curved_edges, connectionstyle='arc3, rad=0.25')
-
+    plt.title(title)
     plt.show()
