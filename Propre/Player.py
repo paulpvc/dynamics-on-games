eba59d4b1c32fa456474bcb57b0b9e6939da0cf2
paulv <paul.vigeolas@gmail.com>
2023-06-01 15:23:45 +0200

    update Player

--- Propre/Player.py
+++ Propre/Player.py
@@ -1,2 +1,9 @@
 from util import *
 import networkx as nx
+
+
+class Player:
+    def __init__(self, name: str, preference: list[set], actions: set):
+        self.name = name
+        self.preference = preference
+        self.actions = actions
