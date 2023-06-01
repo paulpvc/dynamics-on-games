6d8cf94ced34ff852da21c0c863b2723da8e36e9
paulv <paul.vigeolas@gmail.com>
2023-06-01 15:12:01 +0200

    update propre

---
+++ Propre/util.py
@@ -0,0 +1,55 @@
+import networkx as nx
+import my_networkx as my_nx
+import matplotlib.pyplot as plt
+
+
+def affichage(G, title=""):
+    """
+    affichage d'un graphe orienté avec des noms pour les arcs
+    :param G: graphe à afficher (arcs possédant tous des noms)
+    :return: None
+    """
+    pos = nx.spring_layout(G, seed=5)
+    fig, ax = plt.subplots()
+    nx.draw_networkx_nodes(G, pos, ax=ax)
+    nx.draw_networkx_labels(G, pos, ax=ax)
+
+    curved_edges = [edge for edge in G.edges()]
+    nx.draw_networkx_edges(G, pos, edgelist=curved_edges, connectionstyle='arc3, rad=0.25')
+    edge_weights = nx.get_edge_attributes(G, 'w')
+    curved_edge_labels = {edge: edge_weights[edge] for edge in curved_edges}
+    my_nx.my_draw_networkx_edge_labels(G, pos, ax=ax, edge_labels=curved_edge_labels, rotate=False, rad=0.25)
+    plt.title(title)
+    plt.show()
+
+def affichage_dyna(G, title=""):
+    """
+    affichage d'un graphe orienté pour les dynamiques (arcs sans nom)
+    :param G: graphe d'une dynamique à afficher
+    :return: None
+    """
+    pos = nx.spring_layout(G, seed=5, k=5)
+    fig, ax = plt.subplots()
+    nx.draw_networkx_nodes(G, pos, ax=ax)
+    nx.draw_networkx_labels(G, pos, ax=ax)
+
+    curved_edges = [edge for edge in G.edges()]
+    nx.draw_networkx_edges(G, pos, edgelist=curved_edges, connectionstyle='arc3, rad=0.25')
+    plt.title(title)
+    plt.show()
+
+
+def dfs(G: nx.DiGraph, source, seen: dict, cycle: list):
+    seen[source] = True
+    if G.out_degree(source) == 0 and G.in_degree(source) > 0:
+        return True
+    for node in G.neighbors(source):
+
+        if node in cycle:
+            #TODO Verifier que c'est une condition nécéssaire car il existe des graphes pour lesquels Gdis est mineur
+            #Il faut surement rajouter la notion de préférence parce que c'est trop général?
+            return False
+        elif not seen[node]:
+            if dfs(G, node, seen, cycle):
+                return True
+    return False
