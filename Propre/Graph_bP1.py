45103f42cc6c14097328a45354d9ced7177b5cab
paulv <paul.vigeolas@gmail.com>
2023-06-04 19:58:30 +0200

    update bp1

--- Propre/Graph_bP1.py
+++ Propre/Graph_bP1.py
@@ -1,21 +1,34 @@
 from util import *
 import networkx as nx
+from Graph_P1 import GraphP1
 
-class GraphP1:
+class GraphbP1:
     def __init__(self, G: nx.DiGraph, strategies_profiles: list[set]):
         self.G = G
         self.strategies_profiles = strategies_profiles
         self.graph_dyna = self.create_dyna()
 
     def create_dyna(self):
-        dyna_P1 = nx.DiGraph()
+        dyna_bP1 = nx.DiGraph()
         for strategy1 in self.strategies_profiles:
-            for strategy2 in self.strategies_profiles:
-                if self.is_edge(strategy1, strategy2):
-                    dyna_P1.add_edge(get_edge_name(strategy1, self.G), get_edge_name(strategy2, self.G))
-        return dyna_P1
+            best_reply = self.get_best_strategy(strategy1)
+            print(strategy1, best_reply)
+            for better_strategy in best_reply.values():
+                if better_strategy is not None:
+                    dyna_bP1.add_edge(get_edge_name(strategy1, self.G), get_edge_name(better_strategy, self.G))
+        return dyna_bP1
 
-    @staticmethod
+    def get_best_strategy(self, strategy):
+        best_reply = {n: None for n in self.G.nodes()}
+        for strategy2 in self.strategies_profiles:
+            if GraphP1.is_edge(strategy, strategy2):
+                player = strategy.difference(strategy2).pop()[0]
+                if best_reply[player] is None or outcome(player.preference, strategy2) > outcome(player.preference, best_reply[player]):
+                    best_reply[player] = strategy2
+        return best_reply
+
+
+    """@staticmethod
     def is_edge(node1, node2):
         difference = node1.difference(node2)
         if len(difference) == 1:
@@ -23,12 +36,14 @@ class GraphP1:
             if outcome(player.preference, node1) < outcome(player.preference, node2):
                 return True
         return False
+    
+    
     def is_egde_in_bp1(self,node1,node2,strategies):
         if GraphP1.is_edge(node1,node2):
             difference = node1.difference(node2)
 
             for strategy in strategies:
                 if strategy != node2 and GraphP1.is_edge(node1, strategy):
-                    pass
+                    pass"""
     def does_terminate(self):
         return not loop_cycle_detection(self.graph_dyna)
--- Propre/main_test.py
+++ Propre/main_test.py
@@ -1,12 +1,13 @@
 from util import *
 from Graph_P1 import GraphP1
 from Graph_PC import GraphPC
+from Graph_bP1 import GraphbP1
 from Player import Player
 
 G = nx.DiGraph()
 v1 = Player("v1", {"c1","s1"})
 v2 = Player("v2", {"c2","s2"})
-vBot = Player("v3", {})
+vBot = Player("v3", set())
 v1.set_preferences([{(v1,v2), (v2,v1)}, {(v1,vBot)}, {(v1,v2),(v2,vBot)}])
 v2.set_preferences([{(v2,v1), (v1,v2)}, {(v2,vBot)}, {(v2,v1),(v1,vBot)}])
 players = [v1, v2, vBot]
@@ -15,6 +16,7 @@ G.add_nodes_from(players)
 edge_list = [(v1,v2,{"w": "c1"}),(v2,v1,{"w": "c2"}),(v1,vBot,{"w": "s1"}),(v2,vBot,{"w": "s2"})]
 G.add_edges_from(edge_list)
 
-graph_p1 = GraphP1(G, get_strategy_profiles(G))
-graph_pc = GraphPC(G,get_strategy_profiles(G))
-affichage_dyna(graph_pc.graph_dyna)
+#graph_p1 = GraphP1(G, get_strategy_profiles(G))
+#graph_pc = GraphPC(G,get_strategy_profiles(G))
+graph_bp1 = GraphbP1(G, get_strategy_profiles(G))
+affichage_dyna(graph_bp1.graph_dyna)
