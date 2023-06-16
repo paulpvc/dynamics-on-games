ac47afc9f73fae131059b9fc4fe034c294f30826
paulv <paul.vigeolas@gmail.com>
2023-06-16 15:04:29 +0200

    adding support for 2 preference

--- Propre/Player.py
+++ Propre/Player.py
@@ -8,12 +8,14 @@ class Player:
         self.preference = nx.DiGraph()
 
     def set_preferences(self, preference):
-        arcs = get_preference_edges(preference)
-
-        self.preference.add_edges_from(arcs)
-        #affichage_dyna(self.preference, "rjdfodg")
-        self.preference = score(self.preference)
-        #print("score: ",self.preference)
+        if type(preference) == dict:
+            self.preference = preference
+        else:
+            arcs = get_preference_edges(preference)
+            self.preference.add_edges_from(arcs)
+            #affichage_dyna(self.preference, "rjdfodg")
+            self.preference = score(self.preference)
+            #print("score: ",self.preference)
 
     def __repr__(self):
         return str(self.name)
